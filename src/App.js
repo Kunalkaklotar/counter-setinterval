d849222def66461402918869ded187e83ffff66c
Kunalkaklotar <kunalkaklotar@gmail.com>
2023-04-07 22:03:44 +0530

    counter-setinterval

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Timer from "./Timer";
+import { useEffect, useState } from "react";
 
 function App() {
+  let [count, setCount] = useState(10);
+
+  useEffect(() => {
+     const counter = setInterval(() => {
+        setCount(++count);
+     }, 1000);
+
+     return () => {
+        clearInterval(counter);
+     };
+  }, [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Timer countValue={count} />
   );
 }
 
---
+++ src/Timer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Timer(props) {
+   return (
+         <h1>{props.countValue}</h1>
+   )
+}
+
+export default Timer
